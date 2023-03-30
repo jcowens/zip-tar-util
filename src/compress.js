beba799a7f97a06269d426838859b6a0fed5b9ee
jcowens <jeremy7235@gmail.com>
2023-03-29 19:33:17 -0700

    fix pathing issue

--- src/compress.js
+++ src/compress.js
@@ -1,18 +1,22 @@
-import { existsSync, readdirSync } from "fs";
-import { join } from "path";
+import { existsSync, readdirSync, statSync } from "fs";
+import { join, basename } from "path";
 import AdmZip from "adm-zip";
 import { create } from "tar";
+import { promisify } from "util";
+import globModule from "glob";
+
+const glob = promisify(globModule);
 
 /**
- * Compresses a directory into a zip or tar archive
+ * Compresses a directory into a zip, tar, tar.gz or tgz archive
  * @param {string} sourceDir - The path to the source directory
  * @param {string} destFile - The path to the destination archive file
- * @param {string} [archiveType='zip'] - The type of archive to create (zip or tar)
+ * @param {string} [archiveType='zip'] - The type of archive to create (zip, tar, tar.gz or tgz)
  * @returns {Promise<void>}
  */
 async function compress(sourceDir, destFile, archiveType) {
   try {
-    if (archiveType && !["zip", "tar"].includes(archiveType)) {
+    if (archiveType && !["zip", "tar", "tar.gz", "tgz"].includes(archiveType)) {
       throw new Error(`Invalid archive type: ${archiveType}`);
     }
     if (!existsSync(sourceDir)) {
@@ -25,11 +29,23 @@ async function compress(sourceDir, destFile, archiveType) {
         ? destFile
         : `${destFile}.zip`;
       await compressDirToZip(sourceDir, outputFileName);
-    } else {
-      const outputFileName = destFile.endsWith(".tar")
-        ? destFile
-        : `${destFile}.tar`;
-      await compressDirToTar(sourceDir, outputFileName);
+    } else if (
+      archiveType === "tar" ||
+      archiveType === "tar.gz" ||
+      archiveType === "tgz"
+    ) {
+      let outputFileName;
+      if (archiveType === "tar") {
+        outputFileName = destFile.endsWith(".tar")
+          ? destFile
+          : `${destFile}.tar`;
+      } else {
+        outputFileName =
+          destFile.endsWith(".tar.gz") || destFile.endsWith(".tgz")
+            ? destFile
+            : `${destFile}.tar.gz`;
+      }
+      await compressDirToTar(sourceDir, outputFileName, archiveType);
     }
   } catch (error) {
     throw error;
@@ -46,14 +62,18 @@ async function compressDirToZip(sourceDir, destFile) {
   const zip = new AdmZip();
 
   try {
-    // Read all files in the source directory
-    const files = readdirSync(sourceDir);
+    // Read all files and directories in the source directory
+    const entries = readdirSync(sourceDir, { withFileTypes: true });
 
-    // Add each file to the zip archive
-    files.forEach((file) => {
-      const filePath = join(sourceDir, file);
-      zip.addLocalFile(filePath);
-    });
+    // Add each file or directory to the zip archive
+    for (const entry of entries) {
+      const entryPath = join(sourceDir, entry.name);
+      if (entry.isDirectory()) {
+        zip.addLocalFolder(entryPath, entry.name);
+      } else {
+        zip.addLocalFile(entryPath);
+      }
+    }
 
     // Write the zip archive to the destination file
     await zip.writeZip(destFile);
@@ -73,13 +93,28 @@ async function compressDirToZip(sourceDir, destFile) {
  * @returns {Promise<void>}
  */
 async function compressDirToTar(sourceDir, destFile) {
-  await create(
-    {
-      file: destFile,
-      cwd: sourceDir,
-    },
-    readdirSync(sourceDir)
-  );
+  const files = await glob("**/*", {
+    cwd: sourceDir,
+    dot: true,
+    nodir: true,
+    ignore: ["**/.."],
+  });
+
+  try {
+    await create(
+      {
+        file: destFile,
+        cwd: sourceDir,
+      },
+      files
+    );
+  } catch (err) {
+    throw new Error(`Error creating tar file: ${err.message}`);
+  }
+
+  if (!existsSync(destFile)) {
+    console.error(`Tar file ${destFile} not found`);
+  }
 }
 
 export default compress;
