be61bea79e30a2654fdf9b8bc7d7ef663adfb6fc
jcowens <jeremy7235@gmail.com>
2023-03-29 18:14:10 -0700

    removed file

--- test.js
+++
@@ -1,8 +0,0 @@
-import compress from "./src/compress.js";
-
-compress(
-  "C:\\Users\\jere\\Downloads\\SDL-master1",
-  "C:\\Users\\jere\\Downloads\\SDL-master1\\ppppp"
-)
-  .then(() => console.log("Compression complete!"))
-  .catch((err) => console.error(err));
